289ac9a453106fac3cf84bbb6b82993c84bc06cf
Original-goddemon <125777251+Originalgoddemon@users.noreply.github.com>
2023-12-03 19:11:33 +0800

    first commit

---
+++ ccmain.c
@@ -0,0 +1,45 @@
+#define false 0
+#define true 1
+#define _YES 1
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+
+int poliness(void)
+{
+    #ifdef _YES
+	char *pointer=(char*)malloc(14*sizeof(char));
+        if (NULL == pointer)
+	{
+	    pointer=(char*)calloc(14,sizeof(char));
+	    if (NULL == pointer)
+	    {
+		assert(pointer);
+		return -1;
+	    }
+	}
+	char array[14]={"HELLO,MASTER~"};
+	for (int i=0;i<14;++i)
+	{
+	    pointer[i] = array[i];
+	//    printf("%c",pointer[i]);
+	}
+	//putchar('\n');
+	printf("%s\n",pointer);
+	
+    #else
+	printf("hello,Master!");
+    #endif
+}
+
+
+
+
+int main(void)
+{
+    poliness();
+    return 0;
+}
